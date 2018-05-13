98bc92cfb39d36c6f988d3a71435cba005f87725
5252b <5252bb@daum.net>
2018-05-13 22:57:24 +0900

    05.13 22:57 전번 및 이름을 바꾸면 클라이언트에서 값을 바꿉니다. 서버에 올리는 기능은 곧 개발됩니다.

--- app/src/main/java/kr/saintdev/projectmna/views/staff/fragments/main/SettingsFragment.java
+++ app/src/main/java/kr/saintdev/projectmna/views/staff/fragments/main/SettingsFragment.java
@@ -1,16 +1,23 @@
 package kr.saintdev.projectmna.views.staff.fragments.main;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import kr.saintdev.projectmna.R;
+import kr.saintdev.projectmna.modules.staff.lib.dbm.StaffAccountManager;
 import kr.saintdev.projectmna.views.common.SuperFragment;
+import kr.saintdev.projectmna.views.common.dialogs.main.InputTextDialog;
+import kr.saintdev.projectmna.views.staff.activitys.StaffAuthActivity;
+import kr.saintdev.projectmna.views.staff.activitys.StaffMainActivity;
 
 /**
  * Copyright (c) 2015-2018 Saint software All rights reserved.
@@ -21,7 +28,14 @@ import kr.saintdev.projectmna.views.common.SuperFragment;
 public class SettingsFragment extends SuperFragment {
     TextView telView = null;
     TextView nameView = null;
+
+    RelativeLayout[] items = null;
     CircleImageView profileView = null;
+    InputTextDialog nameDialog = null;
+    InputTextDialog telDialog = null;
+
+    StaffMainActivity control = null;
+    StaffAccountManager staffAccount = null;
 
     public SettingsFragment() {
     }
@@ -30,11 +44,76 @@ public class SettingsFragment extends SuperFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragmn_staff_main_settings, container, false);
+        this.control = (StaffMainActivity) getActivity();
 
         this.telView = v.findViewById(R.id.main_settings_content_tel_view);
         this.nameView = v.findViewById(R.id.main_settings_content_name_view);
         this.profileView = v.findViewById(R.id.main_settings_profile_icon);
 
+        this.nameDialog= new InputTextDialog(control, "");
+        this.telDialog = new InputTextDialog(control, "");
+        this.nameDialog.setOnDismissListener(new OnDialogDismissHandler(R.id.main_settings_content_name));
+        this.telDialog.setOnDismissListener(new OnDialogDismissHandler(R.id.main_settings_content_tel));
+        this.nameDialog.setText("이름 변경");
+        this.telDialog.setText("전화번호 변경");
+
+        this.items = new RelativeLayout[]{
+                v.findViewById(R.id.main_settings_content_name),
+                v.findViewById(R.id.main_settings_content_tel),
+                v.findViewById(R.id.main_settings_content_passwd)
+        };
+
+        this.staffAccount = StaffAccountManager.getInstance(control);
+
+        OnButtonClickHandler handler = new OnButtonClickHandler();
+        for(RelativeLayout i : this.items) {
+            i.setOnClickListener(handler);
+        }
+
         return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        this.nameView.setText(this.staffAccount.getValue("staff-name"));
+        this.telView.setText(this.staffAccount.getValue("staff-tel"));
+    }
+
+    class OnButtonClickHandler implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()){
+                case R.id.main_settings_content_tel:
+                    telDialog.setText(staffAccount.getValue("staff-tel"));
+                    telDialog.show();
+                    break;
+                case R.id.main_settings_content_name:
+                    nameDialog.setText(staffAccount.getValue("staff-name"));
+                    nameDialog.show();
+                    break;
+                case R.id.main_settings_content_passwd:
+                    break;
+            }
+        }
+    }
+
+    class OnDialogDismissHandler implements Dialog.OnDismissListener {
+        int id = 0;
+
+        public OnDialogDismissHandler(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            if(id == R.id.main_settings_content_tel) {
+                staffAccount.setValue("staff-tel", telDialog.getData());
+                telView.setText(telDialog.getData());
+            } else if(id == R.id.main_settings_content_name) {
+                staffAccount.setValue("staff-name", nameDialog.getData());
+                nameView.setText(nameDialog.getData());
+            }
+        }
     }
 }
